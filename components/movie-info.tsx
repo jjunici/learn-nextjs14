67767c77b13c6a4233c31b58de26e9cea18fd792
jjunici <nouna12@naver.com>
2025-09-28 15:02:36 +0900

    API_URL Export 제거

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
-import { API_URL } from '../app/(home)/page';
+// import { API_URL } from '../app/(home)/page';
 async function getMovie(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch(`${API_URL}/${id}`);
+  const response = await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies/${id}`);
   return response.json();
 }
 export default async function MoviewInfo({ id }: { id: string }) {
